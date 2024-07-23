90c54e6747553d59fda6c6443991cf9724964a10
Danutz <dan.bilauca@bitstone.eu>
2024-07-23 10:57:44 +0300

    set up client/server images

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
 const express = require('express')
+const fs = require('fs');
+
 const app = express()
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000
 
 const env = process.env.ENV || 'development'
 
-
 console.log('ENV', env);
+
+app.get('/read', (req, res) => {
+    fs.readFile('quotes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error reading data');
+        } else {
+            res.send(JSON.stringify(data));
+        }
+    });
+});
+
+app.post('/write', (req, res) => {
+    const payload = req.body;
+    fs.writeFile('quotes.json', JSON.stringify(payload), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error writing to file');
+        } else {
+            res.send('Payload written to file');
+        }
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.send(`Hello World from ${env} !`);
